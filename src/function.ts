f65e9e152fe936e7fdf911c7bde01bfeaa258c39
Naman Mittal <namanmittal501@gmail.com>
2025-12-21 14:46:48 +0530

    Functions

---
+++ src/function.ts
@@ -0,0 +1,38 @@
+function makeChai(type:string,
+    cups:number
+){
+    console.log(`Making ${type} chai of ${cups} cups.`);
+    
+}
+
+makeChai("Masala chai",5)
+
+function getChaiPrice():number{
+    return 10
+}
+
+function makeOrder(order:string){
+    if(!order) return null
+    return order
+    }
+
+    function logChai(): void{
+        console.log("Chai is ready");
+        
+    }
+
+    // function orderChai(type?: string){
+        
+    // }
+
+    function orderChai(type:string="Masala"){
+
+    }
+
+    function createChai(order:{
+        type:string;
+        sugar:number;
+        size:"small"|"large"
+    }):number{
+return 60
+    }
